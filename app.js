4f4b3012bdf2e45d576ec0126de932bd50c1e196
mostaq786 <60430930+mostaq786@users.noreply.github.com>
2024-02-24 19:52:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,25 @@ require('dotenv').config();
 var port = process.env.PORT || 8080;
 const express = require('express');
 const app = express();
+fs = require('fs');
 
 app.get('/', (req, res) => {
   // Simulating an asynchronous operation
   setTimeout(() => {
     res.write('First response'); // Using res.write() instead of res.send()
+
+    fs.readFile("head.html", function (err, pgres) { 
+			if (err) 
+				res.write("HEAD.HTML NOT FOUND"); 
+			else { 
+				// The following 3 lines 
+				// are responsible for sending the html file 
+				// and ends the response process 
+				res.writeHead(200, { 'Content-Type': 'text/html' }); 
+				res.write(pgres); 
+				res.end(); 
+			} 
+		}); 
 
     // Trying to send another response after a delay
     setTimeout(() => {
